22c7037e9023bc691ffa0176f8e7b2cb4a41b207
Diego Yanez <diegosyanez92@gmail.com>
2018-04-13 14:33:14 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -43,12 +43,12 @@ attr_accessor :total, :discount, :items, :last_total
       # trying to keep track of the last transaction
       @last_total = self.total -= price * quantity
       # need to keep track of a "finished transaction"
-
+      binding.pry
       # when dealing with a transaction is located in "#add_item"
     end
 
     def void_last_transaction
-      @last_total
+      #@last_total
     end
 
 end
